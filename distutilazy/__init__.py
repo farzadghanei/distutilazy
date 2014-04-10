b8e7f28ca512b87f8770efc322573df4c430df86
Farzad Ghanei <farzad.ghanei@gmail.com>
2014-04-10 16:17:14 +0800

    Add distutilazy base dir to sys path when imported

--- distutilazy/__init__.py
+++ distutilazy/__init__.py
@@ -4,6 +4,15 @@ distutility
 Extra distutility commands
 """
 
+import os
+import sys
+
 version = '0.0.2'
 all = ['clean']
 
+base_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+if not base_dir in sys.path:
+    if len(sys.path):
+        sys.path.insert(1, base_dir)
+    else:
+        sys.path.append(base_dir)
