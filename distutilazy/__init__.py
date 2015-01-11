0329cd82fb7335d04da96a90c980428e2ffb4d2e
Farzad Ghanei <farzad.ghanei@gmail.com>
2015-01-11 15:44:01 +0800

    Updated version to 0.3.0

--- distutilazy/__init__.py
+++ distutilazy/__init__.py
@@ -10,7 +10,7 @@
 import os
 import sys
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __all__ = ['clean', 'pyinstaller', 'command']
 
 base_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
