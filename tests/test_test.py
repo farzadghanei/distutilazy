f2453c17bbc4d9ef93df2062396aa781989b05db
Farzad Ghanei <farzad.ghanei@gmail.com>
2015-10-11 12:14:58 +0800

    Refactor test runner tests
    To improve readability of the test

--- tests/test_test.py
+++ tests/test_test.py
@@ -11,22 +11,16 @@ from __future__ import absolute_import
 
 from os.path import dirname, basename
 import sys
-import unittest
+from distutils.dist import Distribution
+from unittest import TestCase, TestSuite, main
 
-sys.path.insert(0, dirname(dirname(__file__)))
-sys.path.insert(0, dirname(__file__))
+here = dirname(__file__)
+sys.path.insert(0, dirname(here))
+sys.path.insert(0, here)
 
 from distutilazy import test
-from distutils.dist import Distribution
 
-# if running in a cached compiled python file
-# assume filename as python source file.
-# when tests are running together, test_clean
-# removes pyc files. but .py file will be available
-_me_ = basename(__file__[:-1] if __file__.endswith('.pyc') else __file__)
-
-if __file__[-1].lower() == 'c':
-    __file__ = __file__[:-1]
+__file__ = basename(__file__[:-1] if __file__.endswith('.pyc') else __file__)
 
 
 def get_module_py_file_names(modules):
@@ -36,15 +30,14 @@ def get_module_py_file_names(modules):
         )
 
 
-class TestTest(unittest.TestCase):
+class TestTest(TestCase):
 
     def test_find_modules_from_package_path(self):
         dist = Distribution()
         test_runner = test.run_tests(dist)
         test_runner.finalize_options()
-        here = dirname(__file__)
         modules = test_runner.find_test_modules_from_package_path(here)
-        self.assertIn(_me_, get_module_py_file_names(modules))
+        self.assertIn(__file__, get_module_py_file_names(modules))
 
     def test_get_modules_from_files(self):
         dist = Distribution()
@@ -52,24 +45,23 @@ class TestTest(unittest.TestCase):
         test_runner.finalize_options()
         self.assertEqual(
             [], test_runner.get_modules_from_files(['none_existing_file']))
-        modules = test_runner.get_modules_from_files([_me_])
+        modules = test_runner.get_modules_from_files([__file__])
         self.assertEqual(1, len(modules))
-        self.assertEqual(_me_, basename(modules.pop().__file__))
+        self.assertEqual(__file__, basename(modules.pop().__file__))
 
     def test_find_test_modules_from_test_files(self):
         dist = Distribution()
-        test_ = test.run_tests(dist)
-        test_.finalize_options()
-        here = dirname(__file__)
-        filename = basename(__file__)
-        modules = test_.find_test_modules_from_test_files(here, 'none_exiting_pattern')
+        test_runner = test.run_tests(dist)
+        test_runner.finalize_options()
+        modules = test_runner.find_test_modules_from_test_files(
+            here, 'none_exiting_pattern')
         self.assertEqual([], modules)
-        modules = test_.find_test_modules_from_test_files(here, filename)
+        modules = test_runner.find_test_modules_from_test_files(here, __file__)
         self.assertEqual(1, len(modules))
-        self.assertEqual(filename, basename(modules.pop().__file__))
-        modules = test_.find_test_modules_from_test_files(here, 'test_*')
+        self.assertEqual(__file__, basename(modules.pop().__file__))
+        modules = test_runner.find_test_modules_from_test_files(here, 'test_*')
         module_names = get_module_py_file_names(modules)
-        self.assertIn(filename, module_names)
+        self.assertIn(__file__, module_names)
         self.assertIn('test_subdir.py', module_names)
 
     def test_test_suite_for_modules(self):
@@ -77,7 +69,7 @@ class TestTest(unittest.TestCase):
         test_ = test.run_tests(dist)
         test_.finalize_options()
         suite = test_.test_suite_for_modules([])
-        self.assertIsInstance(suite, unittest.TestSuite)
+        self.assertIsInstance(suite, TestSuite)
 
     def test_get_test_runner(self):
         dist = Distribution()
@@ -88,4 +80,4 @@ class TestTest(unittest.TestCase):
         self.assertTrue(hasattr(runner.run, '__call__'))
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
