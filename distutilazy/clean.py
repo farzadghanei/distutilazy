ba5070fb68496ace5bb74285fa189feab578ee20
Farzad Ghanei <farzad.ghanei@gmail.com>
2015-10-11 15:48:01 +0800

    Cleared clean moduel, renamed command classes to CamelCase
    PEP8 formatted, and according to convensions rename the command classes
    to upper case. Howerver to keep compatibility, sake_cased class names
    are still supported as aliases for the CamelCase names.

--- distutilazy/clean.py
+++ distutilazy/clean.py
@@ -1,18 +1,16 @@
 """
+distutilazy.clean
+-----------------
 
-    distutilazy.clean
-    -----------------
+command classes to help clean temporary files
 
-    command classes to help clean temporary files
-
-    :license: MIT, see LICENSE for more details.
-
+:license: MIT, see LICENSE for more details.
 """
 
 from __future__ import absolute_import
 
 import os
-import shutil
+from shutil import rmtree
 from distutils import log
 from distutils.core import Command
 from distutils.command import clean
@@ -21,7 +19,8 @@ from . import util
 
 __version__ = "0.4.0"
 
-class clean_pyc(Command):
+
+class CleanPyc(Command):
     description = """Clean root dir from complied python files"""
     user_options = [("root=", "r", "path to root dir")]
 
@@ -32,9 +31,9 @@ class clean_pyc(Command):
 
     def finalize_options(self):
         if not os.path.exists(self.root):
-            raise IOError("Failed to access root path " + self.root)
+            raise IOError("Failed to access root path '{}'".format(self.root))
         self.extensions = [ext.strip() for ext in self.extensions.split(',')]
-        self.directories = [dirname.strip() for dirname in self.directories.split(',')]
+        self.directories = [dir.strip() for dir in self.directories.split(',')]
 
     def find_compiled_files(self):
         """Find compiled Python files recursively in the root path
@@ -43,60 +42,69 @@ class clean_pyc(Command):
         """
         files = []
         for ext in self.extensions:
-            extfiles = util.find_files(self.root, "*." + ext)
-            log.debug("found {0} .{1} files in {2}".format(len(extfiles), ext, self.root))
-            files.extend(extfiles)
-            del extfiles
-        self.announce("found {0} compiled python files in {1}".format(len(files), self.root))
+            ext_files = util.find_files(self.root, "*." + ext)
+            log.debug("found {} '.{}' files in '{}'".format(
+                len(ext_files), ext, self.root)
+            )
+            files.extend(ext_files)
+        self.announce("found '{}' compiled python files in '{}'".format(
+            len(files), self.root))
         return files
 
     def find_cache_directories(self):
         directories = []
-        for dirname in self.directories:
-            dirs = util.find_directories(self.root, dirname)
-            log.debug("found {0} directories in {1}".format(len(dirs), self.root))
+        for dir_name in self.directories:
+            dirs = util.find_directories(self.root, dir_name)
+            log.debug("found {} directories in '{}'".format(
+                len(dirs), self.root))
             directories.extend(dirs)
-            del dirs
-        self.announce("found {0} python cache directories in {1}".format(len(directories), self.root))
+        self.announce("found {} python cache directories in '{}'".format(
+            len(directories), self.root))
         return directories
 
     def _clean_file(self, filename):
-        """Clean a file if exists"""
+        """Clean a file if exists and not in dry run"""
         if not os.path.exists(filename):
             return
-        self.announce("removing " + filename)
+        self.announce("removing '{}'".format(filename))
         if not self.dry_run:
             os.remove(filename)
 
-    def _clean_directory(self, dirname):
-        """Clean a directory if exists"""
-        if not os.path.exists(dirname):
+    def _clean_directory(self, name):
+        """Clean a directory if exists and not in dry run"""
+        if not os.path.exists(name):
             return
-        self.announce("removing directory {0} and all it's contents".format(dirname))
+        self.announce(
+            "removing directory '{}' and all its contents".format(name)
+        )
         if not self.dry_run:
-            shutil.rmtree(dirname, True)
+            rmtree(name, True)
 
     def run(self):
-        dirs = self.find_cache_directories()
-        if dirs:
-            self.announce("cleaning python cache directories in {0} ...".format(self.root))
+        directories = self.find_cache_directories()
+        if directories:
+            self.announce(
+                "cleaning python cache directories in '{}' ...".format(
+                    self.root))
             if not self.dry_run:
-                for dirname in dirs:
-                    self._clean_directory(dirname)
+                for dir_name in directories:
+                    self._clean_directory(dir_name)
 
         files = self.find_compiled_files()
         if files:
-            self.announce("cleaning compiled python files in {0} ...".format(self.root))
+            self.announce(
+                "cleaning compiled python files in '{}' ...".format(self.root))
             if not self.dry_run:
                 for filename in files:
                     self._clean_file(filename)
 
-class clean_all(clean.clean, clean_pyc):
-    description = """Clean root dir from temporary files, complied files, etc."""
+
+class CleanAll(clean.clean, CleanPyc):
+    description = "Clean root dir from temporary files (complied files, etc)"
     user_options = [
-        ("keep-build", None, "do not clean build direcotry"),
-        ("keep-dist", None, "do not clean dist direcotry"),
-        ("keep-egginfo", None, "do not clean egg info direcotry"),
+        ("keep-build", None, "do not clean build directory"),
+        ("keep-dist", None, "do not clean dist directory"),
+        ("keep-egginfo", None, "do not clean egg info directory"),
         ("keep-extra", None, "do not clean extra files"),
     ]
 
@@ -104,7 +112,7 @@ class clean_all(clean.clean, clean_pyc):
 
     def initialize_options(self):
         clean.clean.initialize_options(self)
-        clean_pyc.initialize_options(self)
+        CleanPyc.initialize_options(self)
         self.keep_build = None
         self.keep_dist = None
         self.keep_egginfo = None
@@ -112,7 +120,7 @@ class clean_all(clean.clean, clean_pyc):
 
     def finalize_options(self):
         clean.clean.finalize_options(self)
-        clean_pyc.finalize_options(self)
+        CleanPyc.finalize_options(self)
         self.all = True
 
     def get_egginfo_dir(self):
@@ -124,8 +132,8 @@ class clean_all(clean.clean, clean_pyc):
 
     def clean_egginfo(self):
         """Clean .egginfo directory"""
-        dirname = os.path.join(self.root, self.get_egginfo_dir())
-        self._clean_directory(dirname)
+        dir_name = os.path.join(self.root, self.get_egginfo_dir())
+        self._clean_directory(dir_name)
 
     def clean_dist(self):
         self._clean_directory(os.path.join(self.root, "dist"))
@@ -146,7 +154,7 @@ class clean_all(clean.clean, clean_pyc):
 
     def run(self):
         clean.clean.run(self)
-        clean_pyc.run(self)
+        CleanPyc.run(self)
         if not self.keep_build:
             self.clean_build()
         if not self.keep_egginfo:
@@ -155,3 +163,6 @@ class clean_all(clean.clean, clean_pyc):
             self.clean_dist()
         if not self.keep_extra:
             self.clean_extra()
+
+clean_pyc = CleanPyc
+clean_all = CleanAll
--- tests/test_clean.py
+++ tests/test_clean.py
@@ -18,7 +18,7 @@ here = dirname(__file__)
 sys.path.insert(0, dirname(here))
 sys.path.insert(0, here)
 
-from distutilazy import clean
+from distutilazy.clean import CleanPyc, CleanAll
 
 
 class TestClean(TestCase):
@@ -41,32 +41,32 @@ class TestClean(TestCase):
     def test_clean_all(self):
         dist = Distribution()
         dist.metadata.name = "test_dist"
-        clean_dist = clean.clean_all(dist)
-        clean_dist.finalize_options()
-        self.assertEqual(clean_dist.get_egginfo_dir(), "test_dist.egg-info")
+        all_cleaner = CleanAll(dist)
+        all_cleaner.finalize_options()
+        self.assertEqual(all_cleaner.get_egginfo_dir(), "test_dist.egg-info")
         targets = ["build", "dist", "egginfo", "extra"]
         bad_calls = []
         good_calls = []
         good_calls_should_be = 0
         for target in targets:
-            clean_dist = clean.clean_all(dist)
-            clean_dist.finalize_options()
-            clean_dist.dry_run = True
-            setattr(clean_dist, "keep_%s" % target, True)
-            setattr(clean_dist, "clean_%s" % target,
+            all_cleaner = CleanAll(dist)
+            all_cleaner.finalize_options()
+            all_cleaner.dry_run = True
+            setattr(all_cleaner, "keep_%s" % target, True)
+            setattr(all_cleaner, "clean_%s" % target,
                     lambda x: bad_calls.append(target))
             other_targets = [t for t in targets if t != target]
             for other_target in other_targets:
                 good_calls_should_be += 1
-                setattr(clean_dist, "clean_%s" % other_target,
+                setattr(all_cleaner, "clean_%s" % other_target,
                         lambda x=None: good_calls.append(other_target))
-            clean_dist.run()
+            all_cleaner.run()
         self.assertEqual(bad_calls, [])
         self.assertEqual(len(good_calls), good_calls_should_be)
 
     def test_clean_pyc(self):
         dist = Distribution()
-        pyc_cleaner = clean.clean_pyc(dist)
+        pyc_cleaner = CleanPyc(dist)
         pyc_cleaner.extensions = "ppyycc, ppyyoo"
         pyc_cleaner.finalize_options()
         self.assertEqual(pyc_cleaner.extensions, ["ppyycc", "ppyyoo"])
@@ -74,7 +74,7 @@ class TestClean(TestCase):
 
     def test_clean_py_cache_dirs(self):
         dist = Distribution()
-        pycache_cleaner = clean.clean_pyc(dist)
+        pycache_cleaner = CleanPyc(dist)
         pycache_cleaner.directories = "_test_py_cache_"
         pycache_cleaner.finalize_options()
         self.assertEqual(pycache_cleaner.directories, ["_test_py_cache_"])
@@ -87,7 +87,7 @@ class TestClean(TestCase):
 
     def test_clean_py_cache_dirs_finds_nothing(self):
         dist = Distribution()
-        pycache_cleaner = clean.clean_pyc(dist)
+        pycache_cleaner = CleanPyc(dist)
         pycache_cleaner.extensions = "ppyycc, ppyyoo"
         pycache_cleaner.directories = "not_exist, and_not_found"
         pycache_cleaner.finalize_options()
