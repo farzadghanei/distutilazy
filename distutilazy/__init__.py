1ad9130ed788fcc0d7aadef3365082ce2fa2c78c
Farzad Ghanei <farzad@sam-media.com>
2015-10-13 09:38:00 +0800

    Remove modifying sys.path in package __init__
    it was a mistake I did before, there is no need to modify the path
    in this package.

--- distutilazy/__init__.py
+++ distutilazy/__init__.py
@@ -7,15 +7,5 @@ Extra distutils command classes.
 :license: MIT, see LICENSE for more details.
 """
 
-from os.path import dirname, abspath
-import sys
-
 __version__ = "0.4.0"
 __all__ = ("clean", "pyinstaller", "command")
-
-base_dir = abspath(dirname(dirname(__file__)))
-if base_dir not in sys.path:
-    if len(sys.path):
-        sys.path.insert(1, base_dir)
-    else:
-        sys.path.append(base_dir)
