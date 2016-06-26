950b8432aaaa5ec4f0b717402e5dc701a4a46073
Farzad Ghanei <farzad.ghanei@gmail.com>
2016-06-26 12:18:25 +0200

    Bump version to 0.4.2
     * Fixed hiding import errors while running tests

--- distutilazy/__init__.py
+++ distutilazy/__init__.py
@@ -7,5 +7,5 @@ Extra distutils command classes.
 :license: MIT, see LICENSE for more details.
 """
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 __all__ = ("clean", "command", "pyinstaller", "test")
